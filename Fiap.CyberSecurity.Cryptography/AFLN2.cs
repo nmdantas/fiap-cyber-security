f5d7c85b6573149c0ed880612663dee970d23e69
Logon Aluno <logonrmlocal@fiap.com.br>
2019-03-12 22:41:23 -0300

    Primeiro Commit

---
+++ Fiap.CyberSecurity.Cryptography/AFLN2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+using Fiap.CyberSecurity.Cryptography.Helpers;
+
+namespace Fiap.CyberSecurity.Cryptography
+{
+//     private static BigInteger largePrime(int bitLength, int certainty, Random rnd) {
+//         BigInteger p;
+//         p = new BigInteger(bitLength, rnd).setBit(bitLength-1);
+//         p.mag[p.mag.length-1] &= 0xfffffffe;
+
+//         // Use a sieve length likely to contain the next prime number
+//         int searchLen = getPrimeSearchLen(bitLength);
+//         BitSieve searchSieve = new BitSieve(p, searchLen);
+//         BigInteger candidate = searchSieve.retrieve(p, certainty, rnd);
+
+//         while ((candidate == null) || (candidate.bitLength() != bitLength)) {
+//             p = p.add(BigInteger.valueOf(2*searchLen));
+//             if (p.bitLength() != bitLength)
+//                 p = new BigInteger(bitLength, rnd).setBit(bitLength-1);
+//             p.mag[p.mag.length-1] &= 0xfffffffe;
+//             searchSieve = new BitSieve(p, searchLen);
+//             candidate = searchSieve.retrieve(p, certainty, rnd);
+//         }
+//         return candidate;
+//     }
+
+    public class AFLN2
+    {
+        private 
+        BigInteger p = PrimeHelper.GetNextRandom();
+        BigInteger q = PrimeHelper.GetNextRandom();
+        BigInteger n = p * q;
+
+    }
+}
